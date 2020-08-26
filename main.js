97efab11c0c0c4f8d1081ce748c3aa23f3d68be3
Leo4815162342 <leonid.pyrlya@gmail.com>
2020-08-26 23:06:40 +0200

    Refactor as commonjs

--- main.js
+++ main.js
@@ -1,18 +1,22 @@
-if (typeof define !== 'function') { var define = require('amdefine')(module); }
-define(['./lib/freeze', './lib/LZ', './lib/LZMA', './lib/RangeCoder', './lib/Stream', './lib/Util'], function(freeze, LZ, LZMA, RangeCoder, Stream, Util) {
-  'use strict';
+'use strict';
 
-  return freeze({
-        version: "0.9.0",
-        LZ: LZ,
-        LZMA: LZMA,
-        RangeCoder: RangeCoder,
-        Stream: Stream,
-        Util: Util,
-        // utility methods
-        compress: Util.compress,
-        compressFile: Util.compressFile,
-        decompress: Util.decompress,
-        decompressFile: Util.decompressFile
-  });
+const freeze = require('./lib/freeze');
+const LZ = require('./lib/LZ');
+const LZMA = require('./lib/LZMA');
+const RangeCoder = require('./lib/RangeCoder');
+const Stream = require('./lib/Stream');
+const Util = require('./lib/Util');
+
+module.exports = freeze({
+      version: "0.9.0",
+      LZ: LZ,
+      LZMA: LZMA,
+      RangeCoder: RangeCoder,
+      Stream: Stream,
+      Util: Util,
+      // utility methods
+      compress: Util.compress,
+      compressFile: Util.compressFile,
+      decompress: Util.decompress,
+      decompressFile: Util.decompressFile
 });
